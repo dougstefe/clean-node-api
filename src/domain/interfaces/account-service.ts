7a602b60f7da245251341b8cc4b2a0917a535034
Douglas Lessa <dougstefe@gmail.com>
2021-11-13 17:03:00 -0300

    feat: ensure SignUp call add account

---
+++ src/domain/interfaces/account-service.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from '../models'
+
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AccountService{
+  add (account: AddAccountModel): AccountModel
+}
--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -1,12 +1,15 @@
+import { AccountService } from '../../domain/interfaces/account-service'
 import { InvalidFieldError, RequiredFieldError, ServerError } from '../errors'
 import { badRequest, internalServerError } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
 
 export default class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly accountService: AccountService
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, accountService: AccountService) {
     this.emailValidator = emailValidator
+    this.accountService = accountService
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -17,13 +20,18 @@ export default class SignUpController implements Controller {
           return badRequest(new RequiredFieldError(field))
         }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidFieldError('email'))
       }
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidFieldError('passwordConfirmation'))
       }
+      this.accountService.add({
+        name,
+        email,
+        password
+      })
       return {
         statusCode: 204,
         body: {}
