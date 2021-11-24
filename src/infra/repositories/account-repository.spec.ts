fa9eca0378418185a77598b470a66ea5fa6eb63c
Douglas Lessa <dougstefe@gmail.com>
2021-11-24 00:06:41 -0300

    refactor: move sut creation

--- src/infra/repositories/account-repository.spec.ts
+++ src/infra/repositories/account-repository.spec.ts
@@ -2,14 +2,21 @@ import { Encrypter } from '../interfaces/encrypter'
 import { AccountRepository } from './account-repository'
 
 describe('AccountRepository', () => {
-  class EncrypterStub implements Encrypter {
-    async encrypt (value: string): Promise<string> {
-      return await new Promise(resolve => resolve('encrypted_password'))
+  const makeSut = (): any => {
+    class EncrypterStub implements Encrypter {
+      async encrypt (value: string): Promise<string> {
+        return await new Promise(resolve => resolve('encrypted_password'))
+      }
+    }
+    const encrypterStub = new EncrypterStub()
+    const sut = new AccountRepository(encrypterStub)
+    return {
+      sut,
+      encrypterStub
     }
   }
   test('Should encrypt password', async () => {
-    const encrypterStub = new EncrypterStub()
-    const sut = new AccountRepository(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const addAccountModelMock = {
       name: 'valid_name',
