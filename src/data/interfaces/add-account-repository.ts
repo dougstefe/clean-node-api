c805134a61e3e860843c0f592faee152ca59c26f
Douglas Lessa <dougstefe@gmail.com>
2021-11-28 12:39:11 -0300

    fix: adjust import

--- src/data/interfaces/add-account-repository.ts
+++ src/data/interfaces/add-account-repository.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../domain/models'
-import { AddAccountModel } from '../../domain/usecases/account'
+import { AddAccountModel } from '../../domain/usecases/add-account'
 
 export interface AddAccountRepository {
   add (addAccountModel: AddAccountModel): Promise<AccountModel>
