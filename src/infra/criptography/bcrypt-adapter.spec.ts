7dacbc33323faf66f404b9e379e99964825e2e8f
Douglas Lessa <dougstefe@gmail.com>
2021-11-30 22:11:20 -0300

    refactor: move stu creation to a factor method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,11 +1,27 @@
 import bcrypt from 'bcrypt'
 import { BCryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (data: string, salt: number): Promise<string> {
+    return await new Promise(resolve => resolve('hashed'))
+  }
+}))
+
+const salt = 12
+const makeSut = (): BCryptAdapter => {
+  return new BCryptAdapter(salt)
+}
+
 describe('BCrypt Adapter', () => {
   test('Should call bcrypt', async () => {
-    const sut = new BCryptAdapter(12)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+  test('Should returns hashed', async () => {
+    const sut = makeSut()
+    const result = await sut.encrypt('any_value')
+    expect(result).toEqual('hashed')
   })
 })
