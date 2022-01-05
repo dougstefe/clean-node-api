71fe1953b4dc1c7089addc7869c157882f9f7df9
Douglas Lessa <dougstefe@gmail.com>
2022-01-05 19:30:27 -0300

    refactor: map result helper

--- src/infra/criptography/db/mongodb/account-repository/account.ts
+++ src/infra/criptography/db/mongodb/account-repository/account.ts
@@ -6,8 +6,6 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
-    const response = await accountCollection.insertOne(accountData)
-    console.log(response)
-    return Object.assign({}, accountData, { id: response.insertedId.toString() })
+    return MongoHelper.mapResult(await accountCollection.insertOne(accountData), accountData)
   }
 }
--- src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { Collection, MongoClient } from 'mongodb'
+import { Collection, InsertOneResult, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -10,5 +10,8 @@ export const MongoHelper = {
   },
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+  mapResult (result: InsertOneResult, data: any): any {
+    return Object.assign({}, data, { id: result.insertedId.toString() })
   }
 }
