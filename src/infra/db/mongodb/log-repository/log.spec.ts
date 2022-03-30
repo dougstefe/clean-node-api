d13a5c3417258d68c15d2734189304abea75ec22
Douglas Lessa <dougstefe@gmail.com>
2022-03-29 23:10:37 -0300

    refactor: add makeSut

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -2,6 +2,10 @@ import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LogMongoRepository } from './log'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
 describe('Log Mongo Repository', () => {
   let collection: Collection
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
@@ -14,7 +18,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create an error log', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError(new Error('any_error'))
     const count = await collection.countDocuments()
     expect(count).toBe(1)
