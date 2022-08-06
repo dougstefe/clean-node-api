a9891ba27eef6fdaa4717b12261193bccead8f2e
Douglas Lessa <dougstefe@gmail.com>
2022-08-06 12:56:39 -0300

    refactor: optimize required fields validations

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -7,16 +7,16 @@ export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator, private readonly authentication: Authentication) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new RequiredFieldError(field))
+        }
+      }
       const { email, password } = httpRequest.body
-      if (!email) {
-        return await Promise.resolve(badRequest(new RequiredFieldError('email')))
-      }
-      if (!password) {
-        return await Promise.resolve(badRequest(new RequiredFieldError('password')))
-      }
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
-        return await Promise.resolve(badRequest(new InvalidFieldError('email')))
+        return badRequest(new InvalidFieldError('email'))
       }
       await this.authentication.auth(email, password)
     } catch (error) {
