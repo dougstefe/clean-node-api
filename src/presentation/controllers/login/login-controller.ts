6b5d55fc733f6c988de4c29b22aa3ba5228e67b1
Douglas Lessa <dougstefe@gmail.com>
2022-08-06 12:52:35 -0300

    feat: ensure LoginController calls  Authentication with correct values

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,9 +1,10 @@
+import { Authentication } from '../../../domain/usecases/authentication'
 import { InvalidFieldError, RequiredFieldError } from '../../errors'
 import { badRequest, internalServerError } from '../../helpers/http-helper'
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) { }
+  constructor (private readonly emailValidator: EmailValidator, private readonly authentication: Authentication) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { email, password } = httpRequest.body
@@ -17,6 +18,7 @@ export class LoginController implements Controller {
       if (!isValid) {
         return await Promise.resolve(badRequest(new InvalidFieldError('email')))
       }
+      await this.authentication.auth(email, password)
     } catch (error) {
       return internalServerError(error)
     }
