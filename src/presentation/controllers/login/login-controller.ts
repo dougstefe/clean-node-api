4a247ae2ac568f54aea5d40b2f696ec1c42a713a
Douglas Lessa <dougstefe@gmail.com>
2022-08-06 12:40:25 -0300

    feat: ensure LoginController returns 500 if throws

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,20 +1,24 @@
 import { InvalidFieldError, RequiredFieldError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, internalServerError } from '../../helpers/http-helper'
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (private readonly emailValidator: EmailValidator) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return await Promise.resolve(badRequest(new RequiredFieldError('email')))
-    }
-    if (!password) {
-      return await Promise.resolve(badRequest(new RequiredFieldError('password')))
-    }
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return await Promise.resolve(badRequest(new InvalidFieldError('email')))
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return await Promise.resolve(badRequest(new RequiredFieldError('email')))
+      }
+      if (!password) {
+        return await Promise.resolve(badRequest(new RequiredFieldError('password')))
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return await Promise.resolve(badRequest(new InvalidFieldError('email')))
+      }
+    } catch (error) {
+      return internalServerError(error)
     }
   }
 }
