8928122336a33fe538b069084bb6a52eb6795023
Douglas Lessa <dougstefe@gmail.com>
2022-08-30 21:50:46 -0300

    refactor: rename signup routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
 
   afterAll(async () => await MongoHelper.disconnect())
@@ -12,15 +12,17 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Douglas',
-        email: 'douglas@gmail.com',
-        password: '123456',
-        passwordConfirmation: '123456'
-      })
-      .expect(201)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Douglas',
+          email: 'douglas@gmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+        .expect(201)
+    })
   })
 })
