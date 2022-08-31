88eb810f6ffc34db64ea96e6d6749c183049d768
Douglas Lessa <dougstefe@gmail.com>
2022-08-30 22:55:07 -0300

    test: ensure login route returns 201 on success

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
+import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
@@ -8,12 +12,12 @@ describe('Login Routes', () => {
   afterAll(async () => await MongoHelper.disconnect())
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.client.db().collection('accounts')
+    accountCollection = MongoHelper.client.db().collection('accounts')
     await accountCollection.deleteMany({})
   })
 
   describe('POST /signup', () => {
-    test('Should return 200 on signup', async () => {
+    test('Should return 201 on signup', async () => {
       await request(app)
         .post('/api/signup')
         .send({
@@ -23,6 +27,24 @@ describe('Login Routes', () => {
           passwordConfirmation: '123456'
         })
         .expect(201)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123456', 12)
+      await accountCollection.insertOne({
+        name: 'Douglas',
+        email: 'douglas@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'douglas@gmail.com',
+          password: '123456'
+        })
+        .expect(200)
     })
   })
 })
